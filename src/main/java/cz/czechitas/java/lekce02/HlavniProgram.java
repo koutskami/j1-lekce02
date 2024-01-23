e9012104334e36a235951b6f33b34120c52f750b
koutskami <koutskami@gmail.com>
2024-01-23 20:22:31 +0100

    zelva kresli domecek

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -12,7 +12,28 @@ public class HlavniProgram {
   public void start() {
     zofka = new Turtle();
 
-    //TODO tady bude kód
+    zofka.penDown();
+    zofka.turnRight(90);
+    zofka.move(100);
+    zofka.turnRight(90);
+    zofka.move(100);
+    zofka.turnRight(90);
+    zofka.move(100);
+    zofka.turnRight(90);
+    zofka.move(100);
+    //tohle udelalo ctverec
+    zofka.turnRight(60);
+    zofka.move(60);
+    zofka.turnRight(60);
+    zofka.move(60);
+    //strecha
+    zofka.penUp();
+    zofka.turnLeft(30);
+    zofka.move(30);
+    //zelva poposla doprava a je ready na dalsi ctverec
+    //v dalsim kroku chci udelat z kodu vyse metodu
+    //chtela bych take pomoci cyklu zkratit kod
+
   }
 
 }
