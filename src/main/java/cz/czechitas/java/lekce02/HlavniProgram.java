256f37cbf0d3fa5c9c1bfa8de57c5f47a0eb801a
koutskami <koutskami@gmail.com>
2024-01-30 19:29:45 +0100

    uprava komentaru

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -12,6 +12,7 @@ public class HlavniProgram {
   public void start() {
     zofka = new Turtle();
 
+    //tohle udela ctverec
     zofka.penDown();
     zofka.turnRight(90);
     zofka.move(100);
@@ -21,16 +22,18 @@ public class HlavniProgram {
     zofka.move(100);
     zofka.turnRight(90);
     zofka.move(100);
-    //tohle udelalo ctverec
-    zofka.turnRight(60);
-    zofka.move(60);
-    zofka.turnRight(60);
-    zofka.move(60);
+
     //strecha
+    zofka.turnRight(60);
+    zofka.move(60);
+    zofka.turnRight(60);
+    zofka.move(60);
+
+    //zelva poposla doprava a je ready na dalsi ctverec
     zofka.penUp();
     zofka.turnLeft(30);
     zofka.move(30);
-    //zelva poposla doprava a je ready na dalsi ctverec
+
     //v dalsim kroku chci udelat z kodu vyse metodu
     //chtela bych take pomoci cyklu zkratit kod
 
